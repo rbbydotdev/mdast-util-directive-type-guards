95569a78aac958d21526a66e0c803ac64ccacaa9
Matthew Gibbons <matthew@southwestkoala.co.uk>
2025-01-30 07:48:20 +0000

    Remove dist files from PR

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-export { default as isContainerDirective } from './is-container-directive.js';
-export { default as isDirectives } from './is-directives.js';
-export { default as isLeafDirective } from './is-leaf-directive.js';
-export { default as isTextDirective } from './is-text-directive.js';
